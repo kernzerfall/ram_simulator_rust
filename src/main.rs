5f0226a8f9cba593552fa702c8e0444fa72a5dbc
kernzerfall <25184224+kernzerfall@users.noreply.github.com>
2022-11-18 00:04:10 +0100

    Add second code example to main

--- src/main.rs
+++ src/main.rs
@@ -20,7 +20,33 @@ fn main() {
         End::new()
     );
 
+    let mut isl2 = InstructionVec::new();
+    ivec_push_multiple!(isl2,
+        CLoad::new(0),
+        Store::new(2),
+        CLoad::new(1),
+        Store::new(3),
+        Load::new(3),
+        Sub::new(1),
+        CondJmp::new(Comparison::Gt, 0, 14),
+        Load::new(2),
+        CAdd::new(1),
+        Store::new(2),
+        Load::new(3),
+        CMult::new(2),
+        Store::new(3),
+        Jmp::new(4),
+        Load::new(2),
+        CSub::new(1),
+        Store::new(1),
+        End::new()
+    );
+
     let mut ram = RegisterMachine::new(isl);
     ram.push_vec(vec![2,3]);
+    ram.run();
+
+    ram.load_program(isl2);
+    ram.push_vec(vec![16385]);
     ram.run();
 }
