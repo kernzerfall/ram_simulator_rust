1fd4f1335fe4f28598089961f9cfc2bed381737d
kernzerfall <25184224+kernzerfall@users.noreply.github.com>
2022-11-17 23:20:50 +0100

    Move example to new instructions code

--- src/main.rs
+++ src/main.rs
@@ -1,54 +1,25 @@
-use ram_simulator::{RegisterMachine};
-use ram_simulator::program::{Instruction, InstructionType, Comparison};
+use ram_simulator::*;
+use ram_simulator::instruction::*;
+
+
 
 fn main() {
-    let mut insts: Vec<Instruction> = Vec::new();
-    insts.push(Instruction::new_single(
-        InstructionType::CLoad,
-        1,
-    ));
-    insts.push(Instruction::new_single(
-        InstructionType::Store,
-        3,
-    ));
-    insts.push(Instruction::new_single(
-        InstructionType::Load,
-        2,
-    ));
-    insts.push(Instruction::new_triple(
-        InstructionType::CondJmp,
-        0,
-        Comparison::Eq,
-        10,
-    ));
-    insts.push(Instruction::new_single(
-        InstructionType::CSub,
-        1,
-    ));
-    insts.push(Instruction::new_single(
-        InstructionType::Store,
-        2,
-    ));
-    insts.push(Instruction::new_single(
-        InstructionType::Load,
-        3,
-    ));
-    insts.push(Instruction::new_single(
-        InstructionType::Mult,
-        1,
-    ));
-    insts.push(Instruction::new_single(
-        InstructionType::Store,
-        3,
-    ));
-    insts.push(Instruction::new_single(
-        InstructionType::Jmp,
-        2,
-    ));
-    insts.push(Instruction::end());
+    let mut isl = InstructionVec::new();
+    ivec_push_multiple!(isl, 
+        CLoad::new(1),
+        Store::new(3),
+        Load::new(2),
+        CondJmp::new(program::Comparison::Eq, 0, 10),
+        CSub::new(1),
+        Store::new(2),
+        Load::new(3),
+        Mult::new(1),
+        Store::new(3),
+        Jmp::new(2),
+        End::new()
+    );
 
-    let mut machine = RegisterMachine::new(insts);
-
-    machine.push(vec![2,3]);
-    machine.run();
+    let mut ram = RegisterMachine::new(isl);
+    ram.push_vec(vec![2,3]);
+    ram.run();
 }
