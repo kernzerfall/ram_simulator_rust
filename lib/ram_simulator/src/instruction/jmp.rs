2ed5ad17eaa4a542dc50b8dd229081f1523c4669
kernzerfall <25184224+kernzerfall@users.noreply.github.com>
2022-11-17 23:19:26 +0100

    Implement JMP instructions

---
+++ lib/ram_simulator/src/instruction/jmp.rs
@@ -0,0 +1,49 @@
+use super::Instruction;
+use super::StringRepr;
+use super::{Jmp, CondJmp};
+
+use crate::program::Comparison;
+use crate::state::State;
+
+impl Instruction for Jmp {
+    fn exec(&self, state: &mut State) {
+        state.set_pc(self.operand);
+    }
+}
+
+impl Instruction for CondJmp {
+    fn exec(&self, state: &mut State) {
+        let acc = state.get_acc();
+        if self.comparison.compare(acc, self.value) {
+            state.set_pc(self.jmp_address);
+        } else {
+            state.inc_pc();
+        }
+    }
+
+    
+}
+
+impl CondJmp {
+    pub fn new(comp: Comparison, value: u128, addr: usize) -> CondJmp {
+        CondJmp { comparison: comp, value: value, jmp_address: addr }
+    }
+}
+
+impl StringRepr for CondJmp {
+    fn command_name(&self) -> String { 
+        "COND_JMP".to_string()
+    }
+
+    fn to_string(&self) -> String {
+        let comp: String = match self.comparison {
+            Comparison::Eq => "=",
+            Comparison::Ge => ">=",
+            Comparison::Gt => ">",
+            Comparison::Le => "<=",
+            Comparison::Lt => "<",
+        }.to_string();
+
+        format!("IF c(0){}{} GOTO {}", "<", comp, self.jmp_address)
+    }
+}
