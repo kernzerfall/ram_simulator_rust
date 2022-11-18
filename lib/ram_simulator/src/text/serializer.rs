7c6c8ad41b0538148afd997449e447f51fc9bb29
kernzerfall <25184224+kernzerfall@users.noreply.github.com>
2022-11-18 15:56:28 +0100

    Implement instruction list -> string serialization

--- lib/ram_simulator/src/text/serializer.rs
+++ lib/ram_simulator/src/text/serializer.rs
@@ -1,3 +1,15 @@
 use crate::RegisterMachine;
 
 use super::Serializer;
+
+impl Serializer {
+    pub fn to_string(machine: RegisterMachine) -> String {
+        let mut res = String::new();
+        for inst in machine.program.instructions {
+            res.push_str(inst.to_string().as_str());
+            res.push('\n');
+        }
+
+        res
+    }
+}
