ae62245c53778da9d58f944ae5f48d06bdcf8ca6
kernzerfall <25184224+kernzerfall@users.noreply.github.com>
2022-11-18 18:27:26 +0100

    Add macro to gen matching objects for a str instr

--- lib/ram_simulator/src/text/deserializer.rs
+++ lib/ram_simulator/src/text/deserializer.rs
@@ -1,1 +1,11 @@
 use super::Deserializer;
+macro_rules! gen_single_arg_instr {
+    ($instr:ident, $argtype:ident, $tkarr:expr, $line:expr) => {
+        $instr::new(
+            $tkarr.next()
+                .expect(format!("Line {}: {} needs an argument", $line+1, "$instr").as_str())
+                .parse::<$argtype>()
+                .unwrap()
+        )
+    };
+}
