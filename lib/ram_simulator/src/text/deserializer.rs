c75e336ab2c612c2d073cf46bc20af4b8ce5b362
kernzerfall <25184224+kernzerfall@users.noreply.github.com>
2022-11-18 18:53:22 +0100

    Fix "needs an argument" error display

--- lib/ram_simulator/src/text/deserializer.rs
+++ lib/ram_simulator/src/text/deserializer.rs
@@ -20,7 +20,7 @@ macro_rules! gen_single_arg_instr {
     ($instr:ident, $argtype:ident, $tkarr:expr, $line:expr) => {
         $instr::new(
             $tkarr.next()
-                .expect(format!("Line {}: {} needs an argument", $line+1, "$instr").as_str())
+                .expect(format!("Line {}: {} needs an argument", $line+1, stringify!($instr)).as_str())
                 .parse::<$argtype>()
                 .unwrap()
         )
