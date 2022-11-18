81cd7eb3c756e973199d7cf4ed34a0aebf5c3951
kernzerfall <25184224+kernzerfall@users.noreply.github.com>
2022-11-18 18:29:18 +0100

    Add IF matcher regex

--- lib/ram_simulator/src/text/deserializer.rs
+++ lib/ram_simulator/src/text/deserializer.rs
@@ -1,4 +1,9 @@
 use super::Deserializer;
+lazy_static!{
+    static ref COND_JMP_PARSER: Regex = Regex::new(r"IF\s+[cC]\(0\)\s*(<|>|=|>=|<=)\s*([0-9])\s*THEN\s*GOTO\s*([0-9]*)")
+        .expect("A valid regex");
+}
+
 macro_rules! gen_single_arg_instr {
     ($instr:ident, $argtype:ident, $tkarr:expr, $line:expr) => {
         $instr::new(
