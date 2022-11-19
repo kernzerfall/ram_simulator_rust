f55054b997f0ccc3a8b88e62343f465629de8e59
kernzerfall <25184224+kernzerfall@users.noreply.github.com>
2022-11-19 21:30:31 +0100

    Remove erroneous state/pc dump

--- lib/ram_simulator/src/lib.rs
+++ lib/ram_simulator/src/lib.rs
@@ -73,7 +73,6 @@ impl RegisterMachine {
         }
 
         let pc = self.machine_state.get_pc();
-        print!("Step {:2} -- PC: {:2}, ", self.machine_state.get_steps(), pc + 1);
         self.program.exec_instruction(pc, &mut self.machine_state);
         self.machine_state.inc_steps();
             
