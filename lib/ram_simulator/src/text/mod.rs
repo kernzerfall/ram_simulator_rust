7c3a7a6bb7f3864605dd3551261c79b69e292c52
kernzerfall <25184224+kernzerfall@users.noreply.github.com>
2022-11-19 21:32:07 +0100

    The serializer module is no longer needed

--- lib/ram_simulator/src/text/mod.rs
+++ lib/ram_simulator/src/text/mod.rs
@@ -1,12 +1,8 @@
-mod serializer;
 mod deserializer;
 
 pub trait Serializable {
     fn to_string(&self) -> String;
     fn dump(&self);
-}
-
-pub struct Serializer {
 }
 
 pub struct Deserializer {
