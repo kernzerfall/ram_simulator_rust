e279adc41135d9c938d1c8a4500bac497010397e
kernzerfall <25184224+kernzerfall@users.noreply.github.com>
2022-11-24 19:35:15 +0100

    Rewrite run_machine for global state

--- lib/ram_webassembly/src/lib.rs
+++ lib/ram_webassembly/src/lib.rs
@@ -41,53 +41,37 @@ pub fn reset_machine() {
 
 #[no_mangle]
 #[wasm_bindgen]
+pub unsafe fn run_machine(max_depth: usize) {
+    if !RAM.has_not_ended() {
+        ram_post_res("Machine has already reached an END command", "ramBadResult");
+        return;
+    }
+
+    if RAM.get_state().get_steps() > 0 { 
         ram_post_res(
-            &format!("Continuing from state {}", from_state),
+            &format!("Continuing from state {}", RAM.get_state().to_wasm_comm_str()),
             "ramStateInfo"
         );
-        match State::from_wasm_comm_str(from_state) {
-            Ok(s) => {
-                state = s
-            },
-            Err(u) => {
-                ram_post_res(
-                    &format!("State was neither empty nor valid, err: {}. Starting from initial.", u),
-                    "ramStateInfo"
-                );
+    }
+
+    for _ in 0..max_depth {
+        match &mut RAM.step() {
+            Ok(res_st) => {
+                if !RAM.has_not_ended() {
+                    ram_post_res(&"Machine halted.", "ramGoodResult");
+                    break;
+                }
+
+                ram_post_res(&res_st.to_string(), "");
+        },
+            Err(m_err) => {
+                ram_post_res(m_err, "ramBadResult");
+                return;
             }
         }
     }
-    match deserializer::parse_buf(br) {
-        Ok(m) => {
-                another_ram = m;
-                if state.get_steps() > 0 {
-                    another_ram.set_state(state);
-                }
 
-                for _ in 0..max_depth {
-                    match &mut another_ram.step() {
-                        Ok(res_st) => {
-                            if !another_ram.has_not_ended() {
-                                ram_post_res(&"Machine halted.", "ramGoodResult");
-                                break;
-                            }
-
-                            ram_post_res(&res_st.to_string(), "");
-                    },
-                        Err(m_err) => {
-                            ram_post_res(m_err, "ramBadResult");
-                            ram_update_gl_state(&another_ram.get_state().to_wasm_comm_str());
-                            return;
-                        }
-                    }
-                }
-                ram_update_gl_state(&another_ram.get_state().to_wasm_comm_str());
-                if another_ram.has_not_ended() {
-                    ram_post_res("The machine hasn't halted yet.", "ramStateInfo");
-                }
-        },
-        Err(e) => {
-            ram_post_res(&e, "ramBadResult");
-        }
+    if RAM.has_not_ended() {
+        ram_post_res("The machine hasn't halted yet.", "ramStateInfo");
     }
 }
